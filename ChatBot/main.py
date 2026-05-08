870bdf57ad9ff53c587323f70d904ff3735fee73
heulersilva <heulersilva@yahoo.com.br>
2026-05-08 14:30:38 -0300

    Integration Groq AI and creating .env - security schema to protect key API

--- ChatBot/main.py
+++ ChatBot/main.py
@@ -3,7 +3,10 @@
 from groq import Groq
 import streamlit as st
 
-#Markdown
-st.write("## Chatbot com AI")
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+model_ia = Groq(api_key=os.getenv('KEY'))
 
 text_user = st.chat_input("Digite sua mensagem")
